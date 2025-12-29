2c78f09ea74345cdb91c5c5de0e71752f673f8f2
Brandon Greenwell <greenwbm@Mac.lan>
2025-12-29 14:21:55 -0500

    conductor(checkpoint): Checkpoint end of Phase 3

--- src/pyguide/__init__.py
+++ src/pyguide/__init__.py
@@ -1,3 +1,4 @@
 from .classifier import GuideTreeClassifier
+from .stats import calc_curvature_p_value
 
-__all__ = ["GuideTreeClassifier"]
+__all__ = ["GuideTreeClassifier", "calc_curvature_p_value"]
