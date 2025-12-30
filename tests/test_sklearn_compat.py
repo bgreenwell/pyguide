47619457498cf10dd2207a448add9d79506ea579
Brandon Greenwell <greenwbm@Mac.lan>
2025-12-29 19:16:33 -0500

    test(compat): Pass check_estimator for GuideTreeRegressor

--- tests/test_sklearn_compat.py
+++ tests/test_sklearn_compat.py
@@ -1,9 +1,19 @@
 from sklearn.utils.estimator_checks import check_estimator
 
-from pyguide import GuideTreeClassifier
+from pyguide import GuideTreeClassifier, GuideTreeRegressor
 
 
-def test_sklearn_compatibility():
+
+def test_classifier_compatibility():
+
     # check_estimator runs a suite of tests to ensure the estimator
+
     # adheres to scikit-learn conventions.
+
     check_estimator(GuideTreeClassifier())
+
+
+
+def test_regressor_compatibility():
+
+    check_estimator(GuideTreeRegressor())
